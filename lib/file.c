e1e11e2eb727446b9fee7677c51c2f7793800135
Antonio Cirillo <dev.cirillo@gmail.com>
2022-04-26 18:29:24 +0200

    check input

--- lib/file.c
+++ lib/file.c
@@ -9,7 +9,12 @@ off_t bytes_inside_dir(char* path, GList **list) {
 
     off_t bytes_size = 0;
     struct dirent *de;
-    DIR *dr = opendir(path);
+    DIR *dr;
+    
+    if ((dr = opendir(path)) == NULL) {
+        printf("[%s]: can't open directory\n", path);
+        return -1;
+    }
 
     while ((de = readdir(dr)) != NULL) {
 
@@ -19,11 +24,17 @@ off_t bytes_inside_dir(char* path, GList **list) {
             char *dir_name = de -> d_name;
             // Check if dirent doesn't point on parent or actual directory
             if (strcmp(dir_name, ".") != 0 && strcmp(dir_name, "..") != 0) {
+                
                 // Call bytes_inside_dir on sub directory
                 char *new_path = (char *) malloc(strlen(path) + strlen(dir_name) + 1);
                 sprintf(new_path, "%s/%s", path, dir_name);
-                bytes_size += bytes_inside_dir(new_path, list);
+                 
+                int bytes = bytes_inside_dir(new_path, list);
+                if (bytes > 0)
+                    bytes_size += bytes;
+
                 free(new_path);
+            
             }
 
         } else {
@@ -50,8 +61,10 @@ off_t bytes_of_file(char* path, GList **list) {
 
     // Get bytes size of file
     struct stat st;
-    if (stat(path, &st) == -1)
+    if (stat(path, &st) == -1) {
+        printf("[%s]: can't open file\n", path);
         return -1;
+    }
     off_t size = st.st_size;
 
     // Append new file to list
--- src/word-count.c
+++ src/word-count.c
@@ -5,38 +5,12 @@
 #include "log.h"
 
 #define MASTER 0
-#define INPUT_DIR 1
-#define INPUT_FILES 2
+#define FLAG_DIR 1
+#define FLAG_FILES 2
+
+void check_input(int argc, char **argv, int *operation);
 
 int main (int argc, char **argv) {
-
-    // Check and prepare input
-    int input;
-
-    if (argc < 3) {
-
-        printf("Usage: mpirun -np [num_processors] ./word-count [-d] [path of directory]\n");
-        printf("Usage: mpirun -np [num_processors] ./word-count [-f] [path of file(s)]\n");
-        return EXIT_FAILURE;
-    
-    } else {
-    
-        if (strcmp(argv[1], "-d") == 0)
-            input = INPUT_DIR;
-
-        else if (strcmp(argv[1], "-f") == 0)
-            input = INPUT_FILES;
-        
-        else {
-            
-            printf("word-count: unrecognized operation '%s'\n", argv[1]);
-            printf("Usage: mpirun -np [num_processors] ./word-count [-d] [path of directory]\n");
-            printf("Usage: mpirun -np [num_processors] ./word-count [-f] [path of file(s)]\n");
-            return EXIT_FAILURE;
-
-        }
-
-    }
 
     int rank;                       // id of processor
     int size;                       // number of processors
@@ -45,28 +19,41 @@ int main (int argc, char **argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    // Check number of processors
-    if (size < 2) {
-
-        printf("word-count: you have to run the program with more than one processor\n");
-        return EXIT_FAILURE;
-
-    }
-
     if (rank == MASTER) {
+
+        // Check number of processors
+        if (size < 2) {
+            printf("[word-count]: you have to run the program with more than one processor\n");
+            MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+        }
+
+        // Check input and opeartion mode
+        int operation;
+        check_input(argc, argv, &operation);
 
         GList *file_list = NULL;
         off_t total_bytes = 0;
-        
-        if (input == INPUT_DIR) {
+
+        // Create file list and calculate total of bytes        
+        if (operation == FLAG_DIR) {
             
             total_bytes = bytes_inside_dir(argv[2], &file_list);
-            printf("word-count: %s is empty\n", argv[2]);
-         
+
+            // Check if there is a problem
+            if (total_bytes == -1)
+                MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE); 
+
+            // Check if directory is empty
+            if (total_bytes == 0) {
+                printf("[word-count]: %s is empty\n", argv[2]);
+                MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+            }
+
         } else {
 
-            for (int i = 2; i < argc; i++)
+            for (int i = 2; i < argc; i++) {
                 total_bytes += bytes_of_file(argv[i], &file_list);
+            }
 
         }
 
@@ -79,5 +66,34 @@ int main (int argc, char **argv) {
 
     MPI_Finalize();
     return EXIT_SUCCESS;
+
+}
+
+void check_input(int argc, char **argv, int *operation) {
+
+    if (argc < 3) {
+
+        printf("Usage: mpirun -np [num_processors] ./word-count [-d] [path of directory]\n");
+        printf("Usage: mpirun -np [num_processors] ./word-count [-f] [path of file(s)]\n");
+        MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+    
+    } else {
+    
+        if (strcmp(argv[1], "-d") == 0)
+            *operation = FLAG_DIR;
+
+        else if (strcmp(argv[1], "-f") == 0)
+            *operation = FLAG_FILES;
+        
+        else {
+            
+            printf("[word-count]: unrecognized operation '%s'\n", argv[1]);
+            printf("Usage: mpirun -np [num_processors] ./word-count [-d] [path of directory]\n");
+            printf("Usage: mpirun -np [num_processors] ./word-count [-f] [path of file(s)]\n");
+            MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+
+        }
+
+    }
 
 }
