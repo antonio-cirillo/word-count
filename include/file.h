9da64f4e1c942872ef36d9b6ee8c1eaaec1bdcfd
Antonio Cirillo <dev.cirillo@gmail.com>
2022-04-28 18:22:17 +0200

    added count words function

--- include/file.h
+++ include/file.h
@@ -1,9 +1,12 @@
 #include <stdlib.h>
 #include "glib.h"
 
+#define MAX_PATH_LEN 1024
+#define MAX_WORD_LEN 256
+
 typedef struct file {
 
-    char *path_file;
+    char path_file[MAX_PATH_LEN];
     off_t bytes_size;
 
     long int start_offset;
@@ -14,3 +17,5 @@ typedef struct file {
 off_t bytes_inside_dir(char *, GList **);
 
 off_t bytes_of_file(char *, GList **);
+
+int count_words(char *, int, int);
--- lib/file.c
+++ lib/file.c
@@ -5,6 +5,8 @@
 #include <sys/stat.h>
 #include "file.h"
 
+#define IS_TERMINATOR(ch) ( (ch < 33) ? 1 : 0 )
+
 off_t bytes_inside_dir(char* path, GList **list) {
 
     off_t bytes_size = 0;
@@ -76,11 +78,103 @@ off_t bytes_of_file(char* path, GList **list) {
 
     // Append new file to list
     File *file = malloc(sizeof *file);
-    file -> path_file = path;
+    strncpy(file -> path_file, path, strlen(path));
     file -> bytes_size = bytes_size;
 
     *list = g_list_append(*list, file);
 
     return bytes_size;
+
+}
+
+int count_words(char *path, int start_offset, int end_offset) {
+
+    // Open file
+    FILE *file = fopen(path, "r");
+
+    // Check if there is a problem on opening the file
+    if (file == NULL)
+        return EXIT_FAILURE;
+    
+    char ch;
+
+    // If read doens't start from the begining of file  
+    if (start_offset != 0) {
+
+        // Change position to start offset
+        fseek(file, start_offset, SEEK_SET);
+        ch = fgetc(file);
+
+        if (!IS_TERMINATOR(ch) && ch != EOF) {
+            
+            fseek(file, start_offset - 1, SEEK_SET);
+            ch = fgetc(file);
+
+            // If we start reading a truncate word, skip it
+            if (!IS_TERMINATOR(ch))
+                while (!IS_TERMINATOR(fgetc(file))) ;
+
+        }
+
+    }
+
+    char word[MAX_WORD_LEN];
+    int i = 0;
+
+    // Read word from start to end
+    while (ftell(file) <= end_offset) {
+
+        ch = fgetc(file);
+
+        // Add to buffer
+        if (!IS_TERMINATOR(ch))
+            word[i++] = ch;
+
+        else {
+
+            // Add string terminator
+            word[i] = '\0';
+            printf("Word: %s\n", word);
+            // Reset index
+            i = 0;
+
+            // Skip space
+            while (ftell(file) <= end_offset) {
+                
+                ch = fgetc(file);
+
+                if (!IS_TERMINATOR(ch) || ch == EOF)
+                    break;
+            
+            }
+            
+            // If we finish to read exit
+            if (ftell(file) > end_offset || ch == EOF)
+                break;
+
+            // Retract
+            fseek(file, ftell(file) - 1, SEEK_SET);
+
+        }
+
+    }
+
+    // If we trucate a word
+    if (i > 0) {
+        
+        // Add truncate word
+        ch = fgetc(file);
+        while (!IS_TERMINATOR(ch) && ch != EOF) {
+            word[i++] = ch;
+            ch = fgetc(file);
+        }
+
+        // Add line terminator
+        word[i] = '\0';
+        printf("Word: %s\n", word);
+
+    }
+
+    return EXIT_SUCCESS;
 
 }
