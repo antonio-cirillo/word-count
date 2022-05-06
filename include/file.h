b93e238281719dacca51a8b7b71aac6f266c9959
Antonio Cirillo <dev.cirillo@gmail.com>
2022-05-06 15:26:17 +0200

    update signature

--- include/file.h
+++ include/file.h
@@ -1,21 +1,20 @@
 #include <stdlib.h>
 #include "glib.h"
 
-#define MAX_PATH_LEN 1024
-#define MAX_WORD_LEN 256
+#define MAX_PATH_LEN 256
+#define MAX_WORD_LEN 128
 
 typedef struct file {
 
     char path_file[MAX_PATH_LEN];
     off_t bytes_size;
-
-    long int start_offset;
-    long int end_offset;
+    long start_offset;
+    long end_offset;
 
 } File;
 
-off_t bytes_of_file(char *, GList **);
+off_t bytes_inside_dir(GList **, char *);
 
-off_t bytes_inside_dir(char *, GList **);
+off_t bytes_of_file(GList **, char *);
 
-int count_words(GHashTable **, char *, int, int);
+int count_words(GHashTable **, char *, long, long);
