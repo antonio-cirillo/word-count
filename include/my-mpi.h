877085eef4d49652bafca700a287a0effac453d4
Antonio Cirillo <dev.cirillo@gmail.com>
2022-05-08 22:50:39 +0200

    change send_words signature

--- include/my-mpi.h
+++ include/my-mpi.h
@@ -21,5 +21,5 @@ void send_files_to_slaves(int, GList *, off_t, MPI_Datatype);
 void recv_files_from_master(guint *, File **, MPI_Datatype);
 
 // Functions for merge phase
-void send_words_to_master(GHashTable **, MPI_Datatype);
+void send_words_to_master(GHashTable *, MPI_Datatype);
 void recv_words_from_slaves(int, GHashTable **, MPI_Datatype);
