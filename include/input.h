3934940553829415dde062def07aad7812d8a02b
Antonio Cirillo <dev.cirillo@gmail.com>
2022-05-08 17:01:25 +0200

    add read_files signature

--- include/input.h
+++ include/input.h
@@ -1,4 +1,9 @@
+#include <stdlib.h>
+#include "glib.h"
+
 #define FLAG_DIR 1
 #define FLAG_FILES 2
 
-void check_input(int, char **, int *);
+void check_input(int, char **, int, int *);
+
+void read_files(int, char **, int, int, GList **, off_t *);
